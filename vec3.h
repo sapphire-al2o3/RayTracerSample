fe722f8e83a2bf3cd5eb5758082d1b26468659f9
sapphire-al2o3 <t_telkun@hotmail.com>
2019-01-10 16:04:16 +0900

    add Camera class

--- vec3.h
+++ vec3.h
@@ -103,6 +103,18 @@ Vec3 normalize(const Vec3& v)
     return v / v.length();
 }
 
+// 正規直交基底
+void orthonormalBasis(const Vec3& v1, Vec3& v2, Vec3& v3)
+{
+    if(std::abs(v1.x) > 0.9)
+    {
+        v2 = Vec3(0, 1, 0);
+    }
+
+    v2 = normalize(v2 - dot(v1, v2) * v1);
+    v3 = cross(v1, v2);
+}
+
 // 出力
 std::ostream& operator<<(std::ostream& stream, const Vec3& v)
 {
