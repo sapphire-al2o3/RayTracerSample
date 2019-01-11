bfafdc99d2ca50cc5de5b4fd66f528b117cc42e9
sapphire-al2o3 <t_telkun@hotmail.com>
2019-01-11 20:13:54 +0900

    add Image class

---
+++ image.h
@@ -0,0 +1,50 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <iostream>
+#include <fstream>
+#include "vec3.h"
+#include "util.h"
+
+template <typename T>
+class Image
+{
+public:
+    T** lines;
+    int width;
+    int height;
+
+    Image(int _width, int _height) : width(_width), height(_height)
+    {
+        lines = new T*[height];
+        lines[0] = new T[width * height];
+        for(int i = 1; i < height; i++) {
+            lines[i] = &lines[0][width * i];
+        }
+    }
+
+    ~Image() {
+        delete[] lines[0];
+        delete[] lines;
+    }
+};
+
+void ppm_output(const Image<Vec3>& image, const std::string& filename)
+{
+    std::ofstream file(filename);
+    file << "P3" << std::endl;
+    file << image.width << " " << image.height << std::endl;
+    file << "255" << std::endl;
+    for(int j = 0; j < image.height; j++) {
+        for(int i = 0; i < image.width; i++) {
+            Vec3 c = image.lines[j][i];
+            int r = clamp((int)(255 * c.x), 0, 255);
+            int g = clamp((int)(255 * c.y), 0, 255);
+            int b = clamp((int)(255 * c.z), 0, 255);
+            file << r << " " << g << " " << b << std::endl;
+        }
+    }
+    file.close();
+}
+
+#endif
---
+++ image_test.cpp
@@ -0,0 +1,14 @@
+#include "vec3.h"
+#include "image.h"
+
+int main() {
+    Image<Vec3> img(512, 512);
+    for(int i = 0; i < img.width; i++) {
+        for(int j = 0; j < img.height; j++) {
+            img.lines[j][i] = Vec3(i / (double)img.width, j / (double)img.height, 1.0);
+        }
+    }
+    ppm_output(img, "image_test.ppm");
+
+    return 0;
+}
