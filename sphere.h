fe6cd82e326729b190d47cf82f7e29c8712035cf
tel <t_telkun@hotmail.com>
2019-01-29 01:29:48 +0900

    add cornell box sample

--- sphere.h
+++ sphere.h
@@ -17,6 +17,7 @@ public:
     // Le
     std::shared_ptr<Light> light;
 
+    Sphere(const Vec3& _center, double _radius) : center(_center), radius(_radius) {}
     Sphere(const Vec3& _center, double _radius, const std::shared_ptr<Material>& _material, const std::shared_ptr<Light>& _light) : center(_center), radius(_radius), material(_material), light(_light) {}
 
     bool intersect(const Ray& ray, Hit& res) const
