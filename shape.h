bb4c03cdfdbe0962a5d6fd38a661f1b271875d51
tel <t_telkun@hotmail.com>
2019-02-24 18:10:58 +0900

    fix add default constructor

--- shape.h
+++ shape.h
@@ -15,6 +15,7 @@ public:
     // Le
     std::shared_ptr<Light> light;
 
+    Shape() {}
     Shape(const std::shared_ptr<Material>& _material, const std::shared_ptr<Light>& _light) : material(_material), light(_light) {}
     virtual bool intersect(const Ray& ray, Hit& res) const = 0;
 };
