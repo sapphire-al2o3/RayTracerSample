0131cfa9eb7049ac656b8a2fd85f74ecd728e8f3
tel <t_telkun@hotmail.com>
2019-02-25 00:14:16 +0900

    add sampleDisk function

--- random.h
+++ random.h
@@ -11,4 +11,14 @@ inline double rnd() {
     return dist(mt);
 }
 
+// 半径1の炎上で点を一様サンプリングする
+// x: サンプリングされたx座標の値
+// y: サンプリングされたy座標の値
+void sampleDisk(double& x, double& y) {
+    double u1 = rnd();
+    double u2 = rnd();
+    x = std::sqrt(u1) * std::cos(2 * M_PI * u2);
+    y = std::sqrt(u1) * std::sin(2 * M_PI * u2);
+}
+
 #endif
